ca9dbe44173a3cb1b483ce4aa93c1e34e4d1979d
Spyros Paterakis <spyridon.paterakis@outlook.com.gr>
2022-09-26 21:38:27 +0300

    Introduce export to excel functionality

--- MondialExpenses/Controllers/CashiersController.cs
+++ MondialExpenses/Controllers/CashiersController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MondialExpenses.Data;
@@ -104,6 +105,59 @@ namespace MondialExpenses.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Export(int id)
+        {
+            var cashier = await _context.Cashiers
+                .Include(c => c.Expenses)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Cashier");
+                worksheet.Range(4, 1, 4, 2).Merge();
+                var currentRow = 1;
+                worksheet.Cell(currentRow, 1).Value = "Day";
+                worksheet.Cell(currentRow, 2).Value = "Card";
+                worksheet.Cell(currentRow, 3).Value = "Cash";
+                worksheet.Cell(currentRow, 4).Value = "Sum";
+                worksheet.Cell(currentRow, 5).Value = "Total";
+
+                currentRow++;
+
+                worksheet.Cell(currentRow, 1).Value = cashier.Day.ToString("dd/MM/yyyy");
+                worksheet.Cell(currentRow, 2).Value = string.Format("{0:c}", cashier.Card);
+                worksheet.Cell(currentRow, 3).Value = string.Format("{0:c}", cashier.Cash);
+                worksheet.Cell(currentRow, 4).Value = string.Format("{0:c}", cashier.Sum);
+                worksheet.Cell(currentRow, 5).Value = string.Format("{0:c}", cashier.Total);
+
+                currentRow += 2;
+                worksheet.Cell(currentRow, 1).Value = "Expenses";
+                foreach (var expense in cashier.Expenses)
+                {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = expense.Description;
+                    worksheet.Cell(currentRow, 2).Value = string.Format("{0:c}", expense.Value);
+                }
+
+                worksheet.Column(1).AdjustToContents();
+                worksheet.Row(1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                worksheet.Row(2).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+                worksheet.Row(4).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                worksheet.Row(5).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Left);
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(
+                        content, 
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+                        $"cashier_{cashier.Day.ToString("dd/MM/yyyy")}.xlsx");
+                }
+            }
         }
     }
 }
--- MondialExpenses/Data/ApplicationDbContext.cs
+++ MondialExpenses/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MondialExpenses.Models;
 
 namespace MondialExpenses.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
